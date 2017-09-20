bfcece95bfb69204410d0028c413f87c1b5ba9f5
chadsmith123 <chad.smith123@gmail.com>
2017-09-20 15:49:36 -0700

    init

---
+++ read_xls.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""
+XLSX reader 
+Author: Chad Smith 
+
+Outputs xlsx sheet to std out
+"""
+
+import sys
+import argparse
+from openpyxl import load_workbook
+
+def count(args):
+    wb = load_workbook(filename = args.xlsx)
+    if args.sheet is None:
+        ws = wb[wb.get_sheet_names()[0]]
+    else:
+        ws = wb[args.sheet]
+
+    for row in ws.iter_rows():
+        out=[]
+        for cell in row:
+            dat=str(cell.value)
+            out.append(dat)
+        print(*out, sep=args.delim)
+
+    wb.close()
+if __name__ == '__main__':
+        parser = argparse.ArgumentParser(description='Read xls files')
+        parser.add_argument('-sheet', dest='sheet', type=str, help='sheet number')
+        parser.add_argument('-sep', dest='delim', type=str, default=" ", help='Delimiter')
+        parser.add_argument('xlsx', help='xls file input')
+
+        if len(sys.argv)==1:
+                parser.print_help()
+                sys.exit()
+        args = parser.parse_args()
+        count(args)
