3e1d9f78f6f39e8410beac97254f7a4035225000
chadsmith123 <chad.smith123@gmail.com>
2017-09-20 16:34:31 -0700

    disabled delimiter specification

--- read_xls.py
+++ read_xls.py
@@ -23,13 +23,14 @@ def count(args):
         for cell in row:
             dat=str(cell.value)
             out.append(dat)
-        print(*out, sep=' ')
+        #print(args.delim.join(out))
+        print(*out, sep="\t")
 
     wb.close()
 if __name__ == '__main__':
         parser = argparse.ArgumentParser(description='Read xls files')
         parser.add_argument('-sheet', dest='sheet', type=str, help='sheet number')
-        parser.add_argument('-sep', dest='delim', type=str, default=" ", help='Delimiter')
+        #parser.add_argument('-sep', dest='delim', type=str, default=" ", help='Delimiter')
         parser.add_argument('xlsx', help='xls file input')
 
         if len(sys.argv)==1:
